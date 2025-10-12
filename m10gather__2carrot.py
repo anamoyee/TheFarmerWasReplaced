4b9e0dc80826601228409ed1d65215061fab306c
anamoyee <thecreatorrr333@gmail.com>
2025-10-12 02:57:58 +0200

    wait one more thing

--- m10gather__2carrot.py
+++ m10gather__2carrot.py
@@ -16,7 +16,7 @@ def guarantee_carrot(n):
 	reset_pos()
 
 	while this_num_items() < n:
-		guarantee_amt = 4 * (WS**2)  # 4 (carrots are four in one plot) * (WS**2)
+		guarantee_amt = 8 * (WS**2)  # 4 (carrots are four in one plot) * (WS**2)
 
 		if guarantee_wood(guarantee_amt) or guarantee_hay(guarantee_amt):
 			reset_ground(Grounds.Soil)
